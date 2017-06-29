85ccdfa81cfc46be811d580d6c5e0e7c09ca2243
Jan Parthemore <janparthemore@gmail.com>
2017-06-29 18:25:19 -0400

    successfully created the /weather route that returns weather data for Gainesville

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -1,8 +1,20 @@
 const express = require ('express');
 const router = express.Router(); //meant to be used in constructior w/router
+const darksky = require ('../credentials').darksky; //be careful - want value of property - api key brancelet - get in - key
+//const baseUrl = `https://api.darksky.net/forecast/${darksky}/37.8267,-122.4233`;//don't always want alcatraz
+const baseUrl = `https://api.darksky.net/forecast/${darksky}/`;
+const axios = require('axios'); //by default lllooks in node-Mo
 
 router.get('/weather',(request,response)=>{  //broswer listening for that type of requiest
-  response.send('Here be the weather');
+  //response.send('Here be the weather');
+  const url = `${baseUrl}29,-82`;
+  axios.get(url)
+       .then(weather =>{
+          response.json(weather.data);
+        })
+        .catch(err=>{
+          console.error(err); //wil print to server log (print in heroku)
+        });
 });
 router.get('/weather/:lat,:lon',(request,response)=>{
   response.send('Here be the weather for that place');
